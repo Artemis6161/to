eae0dfbaf21f5f2aac6065ad234c6b38fe630066
sangeetha <Artemis6161@github.com>
2022-01-22 21:58:27 +0530

    org

--- src/component/inputarea.js
+++ src/component/inputarea.js
@@ -1,13 +1,20 @@
-import React,{ useState } from 'react'
-
+import React,{ useState, useRef, useEffect } from 'react'
 const Inputarea = (props) => {
 const [inputText, setInputText] = useState("");
+const inputEl = useRef(null);
 const handlechange = (event) => {
      setInputText(event.target.value);
 };
+console.log(inputEl);
+useEffect(() => {
+  console.log("this is componentDidMount");
+  console.log(inputEl);
+  inputEl.current.focus();
+ 
+}, []);
   return (
     <div className="form">
-        <input type="text" onChange={handlechange} value={inputText} />
+        <input type="text" onChange={handlechange} value={inputText} ref={inputEl}/>
         <button 
         onClick={() => {
             props.addItems(inputText);
